9791374c9c9f2d1222cfe5c6fca78e1e766b35df
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-02-28 14:01:03 +0200

    login page css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
-import "./styles/login.css";
+import styles from "./styles/login.module.css";
 import "./styles/responsive/responsive.css";
 
 const Login = () => {
     return (
         <main>
             <aside>
-                <h2 className="aside-heading">If you have an account, you can:</h2>
-                <ul className="categories">
+                <h2 className={styles["aside-heading"]}>If you have an account, you can:</h2>
+                <ul className={styles["categories"]}>
                     <li>- Watch trailers for free</li>
                     <li>- Share you favourite films</li>
                     <li>- Save the best films to your watchlist</li>
                     <li>and much more...</li>
                 </ul>
             </aside>
-            <section className="content">
-                <form className="login">
+            <section className={styles["content"]}>
+                <form className={styles["login"]}>
                     <h1>Sign In to your account</h1>
                     <label htmlFor="email">Email:</label>
                     <input type="email" name="email" id="email" />
@@ -24,7 +24,7 @@ const Login = () => {
                     <p>
                         You don't have an account? <Link to="/register" replace>Sign Up</Link>
                     </p>
-                    <button className="btn">Sign In</button>
+                    <button className={styles["btn"]}>Sign In</button>
                 </form>
             </section>
         </main>
