17d65ebfd0acbbf2b4f21eca1668304526c3822c
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-03-03 14:23:38 +0200

    change navigation

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import styles from "./styles/navigation.module.css";
 import logo from "./images/logo.png";
+import { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
 
 const Header = () => {
+    const { user } = useContext(AuthContext);
     return (
         // eslint-disable-next-line
         <header className={styles["heading"]}>
-        {/* eslint-disable-next-line */}
+            {/* eslint-disable-next-line */}
             <ul className={styles["heading__navbar"]} role={"list"}>
                 <li className={styles["nav-item-logo"]}>
                     <img src={logo} alt="logo" />
@@ -17,21 +20,29 @@ const Header = () => {
                 <li className={styles["nav-item"]}>
                     <Link to="/catalog">Catalog</Link>
                 </li>
-                <li className={styles["nav-item"]}>
-                    <Link to="/profile">Profile</Link>
-                </li>
-                <li className={styles["nav-item"]}>
-                    <Link to="/create">Create</Link>
-                </li>
-                <li className={styles["nav-item"]}>
-                    <Link to="/login">Sign In</Link>
-                </li>
-                <li className={styles["nav-item"]}>
-                    <Link to="/register">Sign Up</Link>
-                </li>
-                <li className={styles["nav-item"]}>
-                    <Link to="/">Logout</Link>
-                </li>
+                {user.accessToken
+                    ?
+                    <>
+                        <li className={styles["nav-item"]}>
+                            <Link to="/profile">Profile</Link>
+                        </li>
+                        <li className={styles["nav-item"]}>
+                            <Link to="/create">Create</Link>
+                        </li>
+                        <li className={styles["nav-item"]}>
+                            <Link to="/">Logout</Link>
+                        </li>
+                    </>
+                    :
+                    <>
+                        <li className={styles["nav-item"]}>
+                            <Link to="/login">Sign In</Link>
+                        </li>
+                        <li className={styles["nav-item"]}>
+                            <Link to="/register">Sign Up</Link>
+                        </li>
+                    </>
+                }
                 <li className={styles["nav-item-search"]}>
                     <input type="text" placeholder="Search" />
                     <button className={styles["btn-search"]}>Search</button>
