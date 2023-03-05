048cb8e674e08623523f27e60cdc1d73c936e346
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-03-05 13:59:56 +0200

    render film details

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,26 +1,46 @@
 import styles from "./styles/details.module.css";
-import fastX from "../Profile/images/fast-x.jpg";
+import * as service from "../../services/filmService";
+import { AuthContext } from "../../contexts/AuthContext";
 import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
 
 const Details = () => {
     const { publicationId } = useParams();
-    
+    const { user } = useContext(AuthContext);
+    const [film, setFilm] = useState({});
+
+    useEffect(() => {
+        service.getOne(publicationId)
+            .then(result => {
+                setFilm(result);
+            });
+    }, [publicationId]);
+
     return (
         <section className={styles["details"]}>
-            <img className={styles["img-details"]} src={fastX} alt="title" />
+            <img className={styles["img-details"]} src={film.imageUrl} alt={film.title} />
             <article>
-                <h1 className={styles["details-heading"]}>Film Title</h1>
-                <p className={styles["details-category"]}>Film Category</p>
+                <h1 className={styles["details-heading"]}>{film.title}</h1>
+                <p className={styles["details-category"]}>{film.category}</p>
                 <p className={styles["details-content"]}>
-                    Fugitive Dom Torento and ex-cop Brian O'Connor find themselves on the wrong side of the
-                    law in exotic Rio de Janeiro. In Brazil, they must take down a corrupt businessman who
-                    would do anything to keep them dead. A hard-nosed federal agent is back on a mission in
-                    South America, but he soon finds it hard to tell the good guys from the bad guys.
-                    To win their freedom and another high-speed race, they must steal $100 million.
+                    {film.content}
                 </p>
-                <button className={styles["btn-details"]}>Edit</button>
-                <button className={styles["btn-details"]}>Delete</button>
-                <button className={styles["btn-details"]}>Save</button>
+                {user.accessToken
+                    ?
+                    <>
+                        {film._ownerId === user._id
+                            ?
+                            <>
+                                <button className={styles["btn-details-edit"]}>Edit</button>
+                                <button className={styles["btn-details-delete"]}>Delete</button>
+                            </>
+                            :   <button className={styles["btn-details-save"]}>Save</button>
+                        }
+                        {/* <button className={styles["btn-details"]}>Edit</button>
+                        <button className={styles["btn-details"]}>Delete</button> */}
+                    </>
+                    : null
+                }
             </article>
         </section>
     );
