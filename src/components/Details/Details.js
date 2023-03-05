4adf75cf40b75fc9da00664d6249b28c10440924
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-03-05 13:44:49 +0200

    user actions buttons

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,7 +1,10 @@
 import styles from "./styles/details.module.css";
 import fastX from "../Profile/images/fast-x.jpg";
+import { useParams } from "react-router-dom";
 
 const Details = () => {
+    const { publicationId } = useParams();
+    
     return (
         <section className={styles["details"]}>
             <img className={styles["img-details"]} src={fastX} alt="title" />
@@ -15,6 +18,9 @@ const Details = () => {
                     South America, but he soon finds it hard to tell the good guys from the bad guys.
                     To win their freedom and another high-speed race, they must steal $100 million.
                 </p>
+                <button className={styles["btn-details"]}>Edit</button>
+                <button className={styles["btn-details"]}>Delete</button>
+                <button className={styles["btn-details"]}>Save</button>
             </article>
         </section>
     );
--- src/services/filmService.js
+++ src/services/filmService.js
@@ -14,3 +14,4 @@ export const createFilm = (filmData, accessToken) => {
 
 export const getAll = () => fetch(`${baseUrl}/all`).then(res => res.json());
 export const getMyPublications = (ownerId) => fetch(`${baseUrl}/profile/${ownerId}`).then(res => res.json());
+export const getOne = (publicId) => fetch(`${baseUrl}/${publicId}`).then(res => res.json());
