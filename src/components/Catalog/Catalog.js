3d23224037a23da409213df67e42e6d48b4115bf
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-03-17 11:31:11 +0200

    set count for query parameters

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,18 +1,56 @@
 import { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
+import { Link } from "react-router-dom"
 import * as service from "../../services/filmService";
-import PaginationItem from "../Pagination/Pagination";
 import CatalogItem from "./CatalogItem";
 import "./styles/catalog.css";
 
 const Catalog = () => {
     const [films, setFilms] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const location = useLocation();
+    const queryParams = Number(location.search.substring(6));
+    const totalPages = Math.ceil(films.length / 10);
+    const cases = {};
+
+    for (let i = 1; i <= totalPages; i++) {
+        cases[i] = i;
+    }
+
+    console.log(films);
 
     useEffect(() => {
         service.getAll()
             .then(result => {
-                setFilms(result);
+                if (cases[queryParams]) {
+                    setFilms(result);
+                }
             });
     }, []);
+
+    function displayItems() {
+        const startIndex = (currentPage - 1) * 10;
+        const endIndex = startIndex + 10;
+        return films.slice(startIndex, endIndex).map(film => (
+            <li key={film._id}>{film.name}</li>
+        ));
+    }
+
+    // Define a function to display the pagination links
+    function displayPagination() {
+        const totalItems = films.length;
+        const totalPages = Math.ceil(totalItems / 10);
+        const links = [];
+        for (let i = 1; i <= totalPages; i++) {
+            links.push(
+                <Link key={i} to={`/catalog?page=${i}`} onClick={() => setCurrentPage(i)}>
+                    {i}
+                </Link>
+            );
+        }
+        return links;
+    }
 
     //eslint-disable-next-line
     return (
@@ -58,7 +96,11 @@ const Catalog = () => {
                         <h1 className="empty-catalog">There are no publications yet.</h1>
                     }
                 </ul>
-                <PaginationItem />
+                <div className="pagination">
+                    <ul role={"list"}>{displayItems()}</ul>
+                    <div>{displayPagination()}</div>
+                </div>
+
             </section>
         </main>
     );
--- src/components/Home/FilmItem.js
+++ src/components/Home/FilmItem.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import styles from "./styles/main-content.module.css";
 
 const FilmItem = ({
