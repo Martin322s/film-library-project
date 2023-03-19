1c61e0ad77c1e38d9e66f731809696be84b32c9c
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-03-19 16:20:08 +0200

    delete publications by admin

--- src/components/Admin Page/PublicationItem.js
+++ src/components/Admin Page/PublicationItem.js
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import styles from "./styles/admin.module.css";
 
 const AdminPublication = ({
+    _id,
     title,
     imageUrl
 }) => {
@@ -8,7 +10,7 @@ const AdminPublication = ({
         <article className={styles["film-card"]}>
             <img className={styles["film-card-img"]} src={imageUrl} alt={title} />
             <h2 className={styles["film-heading-title"]}>{title}</h2>
-            <button className={styles["link-details-btn"]}>Delete Publication</button>
+            <Link className={styles["delete-details-btn"]} to={`/delete/${_id}`}>Delete Publication</Link>
         </article>
     );
 }
